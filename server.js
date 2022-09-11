9f144610ee1da6515bf81c9c8fb9809b613f32c1
emanuelefavero <emafaux89@gmail.com>
2022-09-11 16:44:54 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const fs = require('fs')
+
+const app = express()
+
+// get data from json file
+const data = JSON.parse(fs.readFileSync('data.json', 'utf8'))
+
+app.get('/', (req, res) => {
+    res.json(data)
+})
+
+data.forEach((item) => {
+    // create a route for each item (using each name or each name in lowercase)
+    // e.g. /john /jack, /John, /Jack
+    app.get(`/${item.name}`, (req, res) => {
+        res.json(item)
+    })
+})
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
